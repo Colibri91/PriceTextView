16ff7535da04e82042b9071d6689d86ea0745558
Rıfat mert Dolar <mert.dolar@appcent.mobi>
2017-07-26 10:30:47 +0300

    - Project is recreated.

---
+++ app/src/main/java/com/mertdolar/pricetextviewproj/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mertdolar.pricetextviewproj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.mertdolar.pricetextview.R.layout.activity_main);
+    }
+}
